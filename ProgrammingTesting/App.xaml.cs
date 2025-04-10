ef5cc38b7439f480cade7a3d9320cdf8fd06abff
John <John@DESKTOP-70PP7PL>
2025-04-10 13:16:02 +0300

    Add project files.

---
+++ ProgrammingTesting/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProgrammingTesting
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
